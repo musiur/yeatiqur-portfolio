3c9043a27f2716a5445263e48a60d5df44983e44
musiur <musiur.opu@gmail.com>
2025-01-04 03:06:11 +0600

    fix build error with quote

--- app/_utils/sections/review.tsx
+++ app/_utils/sections/review.tsx
@@ -4,8 +4,8 @@ const ReviewsSection = () => {
       <h2 className='text-center text-title-h3'>What Clients Are Saying</h2>
       <p className='text-center'>Join 500+ Happy Clients Who Trust Yeatiq!</p>
       <blockquote>
-        "Yeatiq&apos;s strategy transformed my e-commerce store. We hit $99k in
-        sales in just a month!" - Togs Swimwear
+        &quot;Yeatiq&apos;s strategy transformed my e-commerce store. We hit
+        $99k in sales in just a month!&quot; - Togs Swimwear
       </blockquote>
     </div>
   );
